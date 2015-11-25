e35d0531c5d3d5e874f0e6b7dc4c9c7ef3b8bf0d
diedona <diedona@gmail.com>
2015-11-24 23:46:59 -0200

    teste

---
+++ DDona.ArquiteturaTesteSiac.ReportAccess/DataSystemAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDona.ArquiteturaTesteSiac.ReportAccess
+{
+    public class DataSystemAccess
+    {
+        public List<T> Get<T>(string SQL) where T : class
+        {
+            DapperWrapper Dapper = new DapperWrapper();
+            return Dapper.Get<T>(SQL);
+        }
+    }
+}
