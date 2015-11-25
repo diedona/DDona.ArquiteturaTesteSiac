b175b5e340ebb320ba2520c71d4d5e8963c042cf
diedona <diedona@gmail.com>
2015-11-24 23:48:17 -0200

    dapper wrapper

---
+++ DDona.ArquiteturaTesteSiac.ReportAccess/DapperWrapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DDona.ArquiteturaTesteSiac.ReportAccess
+{
+    public class DapperWrapper : IDisposable
+    {
+        private string ConnectionString { get; set; }
+
+        public DapperWrapper(string ConnectionName = "SiacContext")
+        {
+            try
+            {
+                ConnectionString = ConfigurationManager.ConnectionStrings["SiacContext"].ConnectionString;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<T> Get<T>(string SQL) where T : class
+        {
+            using(IDbConnection db = new SqlConnection(this.ConnectionString))
+            {
+                return db.Query<T>(SQL).ToList();
+            }
+        }
+
+
+        public void Dispose()
+        {
+            this.Dispose();
+        }
+    }
+}
